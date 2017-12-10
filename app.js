1c21620f49c6d30647315f768615731d6628903e
Fpini <34208284+Fpini@users.noreply.github.com>
2017-12-10 18:49:29 +0100

    Eliminated console.log in middleware section

--- app.js
+++ app.js
@@ -16,29 +16,20 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-console.log('start middleware');
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-console.log('dev');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-console.log('bodyparser');
 app.use(cookieParser());
-console.log('cookieparser');
 app.use(stylus.middleware(path.join(__dirname, 'public')));
-console.log('stylus');
 app.use(express.static(path.join(__dirname, 'public')));
-console.log('static');
 app.use('/', index);
-console.log('index');
 app.use('/users', users);
-console.log('users');
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
-  console.log('404 :',err);
   next(err);
 });
 
@@ -47,7 +38,6 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-  console.log('error: ',err); 
   // render the error page
   res.status(err.status || 500);
   res.render('error');
