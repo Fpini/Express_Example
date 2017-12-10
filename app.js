c2b2e021dbbaa7a2bb66a51fdae6d0eeb4e6dce3
Fpini <fpinizzotto@gmail.com>
2017-12-10 16:03:40 +0100

    Salvatggio 10 Dicembre 16:04

--- app.js
+++ app.js
@@ -17,20 +17,28 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+console.log('start middleware');
 app.use(logger('dev'));
+console.log('dev');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+console.log('bodyparser');
 app.use(cookieParser());
+console.log('cookieparser');
 app.use(stylus.middleware(path.join(__dirname, 'public')));
+console.log('stylus');
 app.use(express.static(path.join(__dirname, 'public')));
-
+console.log('static');
 app.use('/', index);
+console.log('index');
 app.use('/users', users);
+console.log('users');
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
+  console.log('404 :',err);
   next(err);
 });
 
@@ -39,7 +47,7 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  console.log('error: ',err); 
   // render the error page
   res.status(err.status || 500);
   res.render('error');
